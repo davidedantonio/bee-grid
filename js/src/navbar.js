c8f94ed74c852341245c653e65abbf47dbdc8950
davidedantonio <davide.dantonio@webeetle.com>
2018-06-21 13:59:49 +0200

    fix no layout navbar templates

--- js/src/navbar.js
+++ js/src/navbar.js
@@ -28,26 +28,35 @@ const Events = {
 class Navbar {
 
   constructor(element) {
-    // get element and layout
+    // get element
     var self = this;
     this._element = element;
+
+    // button to toggle menu
+    this._button = document.querySelector('.' + ClassName.BUTTON);
+
+    if (this._element == null) {
+      this._button.style.display = 'none';
+      return false;
+    }
+
+    // get layout
     this._layout = element.parentNode;
 
     // add classes
     this._element.classList.add(ClassName.NAVBAR);
     this._layout.classList.add(ClassName.LAYOUT);
 
-    // add overlay
-    this._overlay = document.createElement('div');
-    this._overlay.classList.add(ClassName.OVERLAY);
-    this._overlay.addEventListener(Events.CLICK,
-      function () {
-          self._toggleNavbar(self);
-      });
-    document.body.appendChild(this._overlay);
-
-    // button to toggle menu
-    this._button = document.querySelector('.' + ClassName.BUTTON);
+    if (this._element != null) {
+      // add overlay
+      this._overlay = document.createElement('div');
+      this._overlay.classList.add(ClassName.OVERLAY);
+      this._overlay.addEventListener(Events.CLICK,
+        function () {
+            self._toggleNavbar(self);
+        });
+      document.body.appendChild(this._overlay);
+    }
 
     // if an element exist add click EVENT
     if (this._button !== null) {
